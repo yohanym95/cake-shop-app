7d46734604fa6ede19faccafa51a243648f6f389
93304 <malshikay@gmail.com>
2021-03-29 10:04:50 +0530

    Add external js file

--- shopapp/wwwroot/js/site.js
+++ shopapp/wwwroot/js/site.js
@@ -2,3 +2,16 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+$(document).ready(function () {
+    //$('#searchBox').autocomplete({
+    //    source: '/api/pie/search'
+    //});
+    AOS.init();
+});
+
+
+$(document).addEventListener("scroll", function () {
+    const header = document.querySelector(".navsticky");
+    header.classList.toggle("sticky", window.scrollY > 0);
+});
